03764732b3c13eccc360a3bd57f43201112e6026
Mhafizakbar <hafizakbar5707@gmail.com>
2025-12-17 21:32:19 +0700

    lagii

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
     'env' => env('APP_ENV', 'local'),
-    'debug' => (bool) env('APP_DEBUG', true),
+    'debug' => true,
     'url' => env('APP_URL', 'http://localhost'),
     'timezone' => 'Asia/Jakarta',
     'locale' => 'en',
